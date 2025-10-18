b088bd5d17995766c970500ef0462732fb17a1c3
lnx <lnx@smail.nju.edu.cn>
2025-10-18 22:09:17 +0800

    update README.md and relevant shell scripts

--- remap.sh
+++ remap.sh
@@ -1,5 +1,38 @@
-design=$1
-shift
+if [[ $# -gt 0 ]]; then
+    option=$1
+    shift
+else
+    echo "usage:"
+    echo "  bash remap.sh <design>"
+    echo "  bash remap.sh all"
+    echo "  bash remap.sh exp <experiment>"
+    exit
+fi
 bash build.sh
 cd install
-python scripts/remap_flow.py test/or_cases/${design}.json $@ 2>&1 | tee ${design}.log
+mkdir -p logs
+case "$option" in 
+    all)
+        all_designs=(ariane133 ariane136 bp bp_be bp_fe bp_multi swerv_wrapper)
+        echo $all_designs
+        for design in ${all_designs[*]}
+        do
+            command=(python scripts/remap_flow.py test/or_cases/${design}.json $@)
+            echo "RUN ${command[*]} 2>&1 | tee logs/${design}.log"
+            "${command[@]}" 2>&1 | tee logs/${design}.log
+        done
+    ;;
+    exp)
+        experiment=$1
+        shift
+        command=(python scripts/remap_flow.py ${experiment}.json $@)
+        echo "RUN ${command[*]} 2>&1 | tee logs/${experiment}.log"
+        "${command[@]}" 2>&1 | tee logs/${experiment}.log
+    ;;
+    *)
+        design=$option
+        command=(python scripts/remap_flow.py test/or_cases/${design}.json $@)
+        echo "RUN ${command[*]} 2>&1 | tee logs/${design}.log"
+        "${command[@]}" 2>&1 | tee logs/${design}.log
+    ;;
+esac
